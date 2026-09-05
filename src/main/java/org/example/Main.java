cfc1212dc1a3f4a606bddc8325d0fce8df7d9b69
Лисунов Владимир <vlisunov@inpolus.ru>
2026-09-05 12:28:45 +0300

    Task conditions added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,47 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+
+    enum DayOfWeek {
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY
+    }
+
+    static class WorkSheet {
+        String name;
+        List<DayOfWeek> workDays;
+
+        public WorkSheet(String name, List<DayOfWeek> workDays) {
+            this.name = name;
+            this.workDays = workDays;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public List<DayOfWeek> getWorkDays() {
+            return workDays;
+        }
+    }
+
+    void test() {
+        var denSheet = new WorkSheet("Денис", List.of(DayOfWeek.TUESDAY, DayOfWeek.THURSDAY, DayOfWeek.SUNDAY));
+        var benSheet = new WorkSheet("Бен", List.of(DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY, DayOfWeek.SATURDAY));
+        var lisSheet = new WorkSheet("Лиза", List.of(DayOfWeek.THURSDAY, DayOfWeek.WEDNESDAY, DayOfWeek.FRIDAY));
+        var sheets = List.of(denSheet, benSheet, lisSheet);
+        // TODO: Необходимо вывести на экран дни в которые работало более 1 человека
+        // sheets.stream()
+        // .forEach(System.out::println);
     }
 }
